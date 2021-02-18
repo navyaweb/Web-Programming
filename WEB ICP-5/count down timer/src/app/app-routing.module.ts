b67f2b38b5509f81c3f588314723569636b34ae9
navyaweb <78056182+navyaweb@users.noreply.github.com>
2021-02-17 19:25:17 -0600

    commit

---
+++ WEB ICP-5/count down timer/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CountdownComponent } from './countdown/countdown.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'countdown', pathMatch: 'full'},
+
+  {
+    path: 'countdown',
+    component: CountdownComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
